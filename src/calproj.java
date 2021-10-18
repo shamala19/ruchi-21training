2a241dc79a4cdc79233f207a9e0e8ddd1da221cd
Ruchitha P <ruchithap@most.com>
2021-10-18 12:12:48 +0530

    second commit

---
+++ src/calproj.java
@@ -0,0 +1,28 @@
+public class calproj{
+
+    public static void main(String args[]){
+
+        int num1 = 15, num2 = 2, sum,sub,mul,div;
+        sum=num1 + num2;
+        sub=num1 - num2;
+        mul=num1 * num2;
+        div=num1 / num2;
+        System.out.println("Sum of these numbers: " +sum + " " +sub + "" +mul + " " +div);
+
+        int a = 8;
+        int b = 25;
+        int c = 0;
+        c = a & b;
+        System.out.println("a & b = " + c );
+        c = a / b;
+        System.out.println("a / b = " + c );
+        c = b ^ a;
+        System.out.println("a ^ b = " + c );
+        c = ~a;
+        System.out.println("~a = " + c );
+
+
+
+
+    }
+}
