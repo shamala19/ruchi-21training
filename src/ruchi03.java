56b6c35cba34e41fb1c005e7eea7e57bcd56fbb4
Ruchitha P <ruchithap@most.com>
2021-10-18 13:53:32 +0530

    new commit

---
+++ src/ruchi03.java
@@ -0,0 +1,11 @@
+public class ruchi03 {
+    public static void main(String args[]) {
+        int number = -2;
+        if (number > 1) {
+            System.out.println("positive");
+        } else {
+            System.out.println("negative");
+        }
+    }
+
+}
